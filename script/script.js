ee0df7b9a3e1a8240d685bab624d576bdf52d159
nayyyhaa <nehaguptang.ng@gmail.com>
2021-05-12 18:50:18 +0530

    feat: keydown event

--- script/script.js
+++ script/script.js
@@ -6,16 +6,35 @@ const keys = document.querySelectorAll('.key');
 const whiteKeys = document.querySelectorAll('.key.white');
 const blackKeys = document.querySelectorAll('.key.black');
 
+/*Play on click */
 keys.forEach(key => {
-    key.addEventListener("click", () => {
-        let noteAudio = document.getElementById(key.dataset.note);
-        // console.log(key.dataset.note) -> C,D
-        // console.log(noteAudio) -> audio note is selected
-        noteAudio.currentTime = 0; //play it fast
-        noteAudio.play();
-        key.classList.add('active');
-        noteAudio.addEventListener("ended", () =>{
-            key.classList.remove('active');
-        })
-    })
+    key.addEventListener("click", () => play(key))
 })
+
+/*Play via keyboard*/
+document.addEventListener("keydown", (e)=>{
+    //stop multiple keydown
+    if (e.repeat) return;
+    
+    // console.log(e);  //Keyboard event object with (prop: key = z,x)*/ 
+    let key = e.key;
+
+    let whiteKeyIndex = WHITE_KEYS.indexOf(key);
+    let blackKeyIndex = BLACK_KEYS.indexOf(key);
+
+    if(whiteKeyIndex > -1) play(whiteKeys[whiteKeyIndex]);
+    if(blackKeyIndex > -1) play(blackKeys[blackKeyIndex]);
+
+})
+
+function play(key) {
+    let noteAudio = document.getElementById(key.dataset.note);
+    // console.log(key.dataset.note) -> C,D
+    // console.log(noteAudio) -> audio note is selected
+    noteAudio.currentTime = 0; //play it fast
+    noteAudio.play();
+    key.classList.add('active');
+    noteAudio.addEventListener("ended", () =>{
+        key.classList.remove('active');
+    })
+}
